737f2c0bede4655a3251394dfbb9c85007c1bf7b
Elena <2890715+LenaRib@users.noreply.github.com>
2021-06-11 00:02:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * Create a class for the Backpack object type.
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ */
+import Backpack from "./Backpack.js";
+import Book from "./Book.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false
+);
+
+console.log("The everydayPack object:", everydayPack);
+console.log("The pocketNum value:", everydayPack.pocketNum);
+
+const book = new Book(
+  'black swan',
+  'Nassim Taleb',
+  14.99,
+  'CoLibri',
+  712,
+  56,
+  true
+);
+
+console.log('The Book object ' book);
