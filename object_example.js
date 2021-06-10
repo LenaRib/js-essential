fc9e97f0b5cdeecb1acf2cb9d03a82885c006c3a
Elena <2890715+LenaRib@users.noreply.github.com>
2021-06-10 23:30:59 +0200

    Update object_example.js

--- object_example.js
+++ object_example.js
@@ -8,12 +8,18 @@ const backpack = {
     right: 26,
   },
   lidOpen: false,
+  empty: true,
+  content: {},
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
+  },
+  whatInside: function (nastedObj) {
+    if (nastedObj) this.empty = false;
+    this.content = nastedObj;
   },
 };
 
