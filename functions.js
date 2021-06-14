303a4f0946d7cb76e663bba0957ecff243b1d463
Elena <2890715+LenaRib@users.noreply.github.com>
2021-06-14 06:14:12 +0200

    Create functions.js
    add standard function

---
+++ functions.js
@@ -0,0 +1,22 @@
+const greenPack = {
+  name: "Frog Pack",
+  color: "green",
+  volume: 8,
+  pocketNum: 3,
+};
+
+const addPack = function (currentPack) {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = `
+    <h1>${currentPack.name}</h1>
+    <ul>
+      <li>Volume: ${currentPack.volume}</li>
+      <li>Color: ${currentPack.color}</li>
+      <li>Number of pockets: ${currentPack.pocketNum}</li>
+    </ul>
+  `;
+  return newArticle;
+};
+
+const main = document.querySelector("main");
+main.append(addPack(greenPack));
